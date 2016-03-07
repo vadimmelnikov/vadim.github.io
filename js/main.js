ad2043afc999ccaf5f7335b88898f4e8c722c7bb
VadimMelnikov <vadic_m@mail.ru>
2016-03-07 05:24:13 +0300

    Commit filter

--- js/main.js
+++ js/main.js
@@ -1,25 +1,31 @@
-$( document ).ready(function() {
-	$(function() {
-	    //----- OPEN
-	    $('[data-popup-open]').on('click', function(e)  {
-	        var targeted_popup_class = jQuery(this).attr('data-popup-open');
-	        $('[data-popup="' + targeted_popup_class + '"]').bPopup({
-                    position: [0, 20]
-                });
-	   
-	 
-	        e.preventDefault();
-	    });
-	});
-	$.fn.replaceUrl = function(){
-	    var regexp = /((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
-	    this.each(function(){
-	    $(this).html(
-	    $(this).html().replace(regexp,'<a href="$1" target="windowName">$1</a>'));
-	 });
-	    return $(this);
-	}
-// например, ищем ссылки в див'е mydiv
+$(document).ready(function() {
+    $(function() {
+        //----- OPEN
+        $('[data-popup-open]').on('click', function(e) {
+            var targeted_popup_class = jQuery(this).attr('data-popup-open');
+            $('[data-popup="' + targeted_popup_class + '"]').bPopup({
+                position: [0, 20]
+            });
+
+
+            e.preventDefault();
+        });
+    });
+    $.fn.replaceUrl = function() {
+            var regexp = /((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
+            this.each(function() {
+                $(this).html(
+                    $(this).html().replace(regexp, '<a href="$1" target="windowName">$1</a>'));
+            });
+            return $(this);
+        }
+        // например, ищем ссылки в див'е mydiv
     $('.popup').replaceUrl();
+
+    // Filter
+
+    //You can override any of these options and then call...
+    	$('.portfolio-list').mixItUp();
+
 
 });
